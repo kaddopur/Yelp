5d73e6c4c71c16430434aebd12f553967fe240a9
Jason Huang <cjhuang@yahoo-inc.com>
2015-06-21 02:46:30 +0800

    add filter view controller

---
+++ Yelp/FilterViewController.h
@@ -0,0 +1,15 @@
+//
+//  FilterViewController.h
+//  Yelp
+//
+//  Created by Jason Huang on 6/21/15.
+//  Copyright (c) 2015 Jason Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterViewController : UIViewController
+
+- (IBAction)doneButtonClicked:(id)sender;
+
+@end
